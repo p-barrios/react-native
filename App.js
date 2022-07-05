3d833dcdb43406d822b88a73aaf0f8102f41622c
Barrios <pbarrios@raffo.com.ar>
2022-07-04 23:24:56 -0300

    Creacion del proyecto

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+       <Text>Hola, Coder!</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 30,
+    paddingTop:50,
+    paddingBottom: 100
   },
 });
